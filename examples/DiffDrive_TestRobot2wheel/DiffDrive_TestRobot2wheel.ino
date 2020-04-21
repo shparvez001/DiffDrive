f5612f1ee26aae94906ebbc8d3428258eabd62cc
Shahadat Hussain Parvez <shparvez001@gmail.com>
2020-04-21 08:21:23 +0600

    Tidying up the examples

--- examples/DiffDrive_TestRobot2wheel/DiffDrive_TestRobot2wheel.ino
+++ examples/DiffDrive_TestRobot2wheel/DiffDrive_TestRobot2wheel.ino
@@ -12,29 +12,18 @@ DiffDrive robot(M2DIRA, M2DIRB, M1DIRA, M1DIRB);
 
 void setup()
 {
-
- 
-        robot.forward();
-        delay(200);
-        robot.reverse();
-        delay(200);
-        robot.right();
-        delay(200);
-        robot.left();
-        delay(200);
-
-    
-
-
-    // initialize serial communication at 9600 bits per second:
-    //Serial.begin(9600);
-
+	robot.forward();
+	delay(200);
+	robot.reverse();
+	delay(200);
+	robot.right();
+	delay(200);
+	robot.left();
+	delay(200);
 }
 
 void loop()
-{
- 
-    
+{    
     robot.forward();
 }
 
--- examples/DiffDrive_TestRobot4wheel/DiffDrive_TestRobot4wheel.ino
+++ examples/DiffDrive_TestRobot4wheel/DiffDrive_TestRobot4wheel.ino
@@ -2,43 +2,33 @@
 
 
 // Motor Control. Change for hardware specific
-int M2DIRA = 4;
-int M2DIRB = 5;
-int M1DIRA = 6;
-int M1DIRB = 7;
-int M4DIRA = 8;
-int M4DIRB = 9;
-int M3DIRA = 10;
-int M3DIRB = 11;
+int M4DIRA = 4;
+int M4DIRB = 5;
+int M3DIRA = 6;
+int M3DIRB = 7;
+int M2DIRA = 8;
+int M2DIRB = 9;
+int M1DIRA = 10;
+int M1DIRB = 11;
 
-DiffDrive robot(M2DIRA, M4DIRB, M3DIRA, M3DIRB, M2DIRA, M2DIRB, M1DIRA, M1DIRB);
+
+DiffDrive robot(M4DIRA, M4DIRB, M3DIRA, M3DIRB, M2DIRA, M2DIRB, M1DIRA, M1DIRB);
 
 
 void setup()
 {
-
- 
-        robot.forward();
-        delay(200);
-        robot.reverse();
-        delay(200);
-        robot.right();
-        delay(200);
-        robot.left();
-        delay(200);
-
-    
-
-
-    // initialize serial communication at 9600 bits per second:
-    //Serial.begin(9600);
-
+    robot.forward();
+    delay(200);
+    robot.reverse();
+    delay(200);
+    robot.right();
+    delay(200);
+    robot.left();
+    delay(200);
 }
 
 void loop()
-{
- 
-    
+{    
     robot.forward();
 }
 
